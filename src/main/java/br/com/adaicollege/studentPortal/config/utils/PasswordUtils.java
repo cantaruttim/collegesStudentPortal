4fea7d251e3183129555923f0a4f2327f8b0fed0
cantaruttim <cantaruttim@outlook.com>
2025-12-14 23:09:31 -0300

    default password

--- src/main/java/br/com/adaicollege/studentPortal/config/utils/PasswordUtils.java
+++ src/main/java/br/com/adaicollege/studentPortal/config/utils/PasswordUtils.java
@@ -3,7 +3,7 @@ package br.com.adaicollege.studentPortal.config.utils;
 import java.time.LocalDate;
 
 public class PasswordUtils {
-    public static String defaultPassword(String registrationNumber) {
+    public static String defaultPassword(String registrationNumber, String course) {
 
         if (registrationNumber == null || registrationNumber.length() < 4) {
             throw new IllegalArgumentException("Invalid registration number");
@@ -12,6 +12,12 @@ public class PasswordUtils {
         LocalDate today = LocalDate.now();
         int year = today.getYear();
         int month = today.getMonthValue();
+
+        // course enrolled
+        String courseEnrolled = course.substring(
+                // change this logic
+                course.length() - 2
+        );
 
         String lastDigits = registrationNumber.substring(
                 registrationNumber.length() - 4
