e1829f8bab8a4b41bb0cb276922860fdaac61fbf
cantaruttim <cantaruttim@outlook.com>
2025-12-25 08:57:58 -0300

    updated

--- src/main/java/br/com/adaicollege/studentPortal/config/security/auth/WebSecurityConfig.java
+++ src/main/java/br/com/adaicollege/studentPortal/config/security/auth/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests( (auth) -> {
-                            auth.requestMatchers(new AntPathRequestMatcher("/user-login", "GET")).authenticated()
+                            auth.requestMatchers(new AntPathRequestMatcher("/student-login", "POST")).authenticated()
                                     .anyRequest().permitAll();
 
                     })
